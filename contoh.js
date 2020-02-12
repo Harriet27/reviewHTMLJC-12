3a7a47c14be8db6fdf01f22b9301a18b0faeacc8
Firliandy <lian.eddy@gmail.com>
2020-02-12 11:39:10 +0700

    duplicate

--- contoh.js
+++ contoh.js
@@ -12,4 +12,38 @@ var newObj = {
     nama : 'susilo'
 }
 
+// console.log(newObj)
+
+var obj = {
+    nama: 'lian'
+}
+// console.log(obj)
+
+obj.nama = 'andi'
+// console.log(obj)
+
+
+var arr = [
+    {namaDepan: 'susil', usia : [2,3]},
+    {namaDepan: 'jokoo', usia : [2,3,5]},
+    {namaDepan: 'lian', usia : [2,3,5,6]},
+]
+var contoh = arr.findIndex((val) => {
+    return val.usia.length === 2
+})
+
+
+
+var obj = {
+    nama: 'lian',
+    usia : 23
+}
+
+var newObj = {
+    ...obj,
+    count : 1
+}
+
+var arr = [1,2,3]
+var newArr = [...arr,4,5,6]
 console.log(newObj)
